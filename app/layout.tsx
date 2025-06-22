2666797cbecd989e4331ffeddab917cccdde89ef
Shahrukh Azhar Ahsan <shahrukh.azhar44@gmail.com>
2025-06-22 22:43:17 +0200

    Reusable modal - TBD

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
+import Modal from "./components/Modals/Modal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (<p>Hi there!</p>)
+  
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
@@ -31,6 +34,7 @@ export default function RootLayout({
         <div className="pt-32">
           {children}
         </div>
+
       </body>
     </html>
   );
