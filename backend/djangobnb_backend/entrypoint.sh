1c654c305a22376157bb3352952ba2e44f862fc1
Shahrukh Azhar Ahsan <shahrukh.azhar44@gmail.com>
2025-07-05 02:11:33 +0200

    Dockerfile added

---
+++ backend/djangobnb_backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "The database is up and running :)"
+
+fi
+
+
+python manage.py migrate
+exec "$@"
+    
